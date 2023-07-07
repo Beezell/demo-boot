b1b4de6b89f348500cf5c6297b29dc221b7506fb
vagrant <vagrant@dev>
2023-07-07 10:00:30 +0000

    adding unused import

--- src/main/java/com/formation/service/Calculator.java
+++ src/main/java/com/formation/service/Calculator.java
@@ -1,5 +1,10 @@
 package com.formation.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+
 import org.springframework.stereotype.Service;
 
 @Service
